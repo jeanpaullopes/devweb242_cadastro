ad5ffb303a3ae7568a56260904ad165a8f200ebd
jeanpaullopes <jean.lopes@ulife.com.br>
2024-06-04 20:19:05 -0300

    services index

---
+++ src/services/index.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+console.log(process.env.url);
+console.log(process.env);
+
+const jsonAPI = axios.create({
+  baseURL: process.env.URL,
+});
+const services = {
+  /*
+  getPosts: (funcaoParaRetornar) => {
+    jsonPlaceHolder.get('posts')
+    .then( (retorno) => {
+      funcaoParaRetornar(retorno.data);
+    } ).catch( (erro) => {
+      this.mensagemErro(erro);
+    });
+  },
+  getUsers: (callback) => {
+    jsonPlaceHolder.get('users').then( (retorno) => {
+     callback(retorno.data);
+    } ).catch( (erro) => {
+      this.mensagemErro(erro);
+    })
+  },
+  */
+  mensagem: (msg) => {
+    Notify.create({
+      message: msg,
+      color: "primary",
+      position: "bottom",
+      timeout: 3000,
+    });
+  },
+  mensagemErro: (msg) => {
+    Notify.create({
+      message: msg,
+      color: "negative",
+      position: "bottom",
+      timeout: 3000,
+    });
+  },
+};
+
+export default services;
